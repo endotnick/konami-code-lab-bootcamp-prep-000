abf0551ec152ecc46a89503f3ae172d0b13d94cb
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-15 20:21:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,35 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+  38,
+  38,
+  40,
+  40,
+  37,
+  39,
+  37,
+  39,
+  66,
+  65
 ];
+
+let input = [];
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(key)) {
+    input.push(key.which);
+    if (input.length > 10) {
+      input.shift;
+    }
+    if (input.length === 10) {
+      let index = 0;
+      do {
+        if (input[index] === codes[index]) {
+          index++;
+        } else {
+          index = -1;
+        }
+      } while(index != -1)
+    }
+  }
+
 }
